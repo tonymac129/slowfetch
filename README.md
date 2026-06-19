b9eb4445e87dd882bc08c0abec0abc773ef30605
tonymac129 <tonyhsu129@gmail.com>
2026-06-18 20:28:22 -0400

    feat: add memory, cpu, gpu, shell, and readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Slowfetch - a simple, lightweight alternative for Fastfetch/Neofetch!
+
+Slowfetch is a simple CLI tool that reads your device's hardware, operating system, and runtime information and outputs them in a clean, organized, and visually appealing list so you can access the statistics quickly! (plus, you also get to flex the beautiful, glorious, majestic, perfect, and breathtaking logo of your operating system!)
+
+## Features
+
+- Displays the logged in user's username and the device's hostname
+- Displays the OS and Kernel names and versions
+- Displays current uptime in a readable format
+- More coming soon!
+
+## Getting started
+
+1. Clone this project with `git clone https://github.com/tonymac129/slowfetch.git`
+2. Make sure you have Rust, Cargo, and other necessary build tools installed
+3. Change into the directory, and run `cargo run`
+4. You should be able to see the output of Slowfetch!
+
+## Tech stack
+
+- Rust for the core logic since this is supposed to be a ~~slow~~ fast fetching program 🦀
+- The std library to interact with the system and owo-colors for color coding
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use owo_colors::OwoColorize;
-use std::{env, fs};
+use std::{env, fs, process::Command};
 
 fn main() {
     let username: String = if let Ok(user) = env::var("USER") {
@@ -40,6 +40,62 @@ fn main() {
     let u_hours: String = calc_time(&mut uptime, 3600.0, "hour");
     let u_minutes: String = calc_time(&mut uptime, 60.0, "minute");
     let u_seconds: String = calc_time(&mut uptime, 1.0, "second");
+    let memory: String = fs::read_to_string("/proc/meminfo").expect("Found file!");
+    let mut total_mem: f32 = 0.0;
+    let mut available_mem: f32 = 0.0;
+    for line in memory.lines() {
+        if line.starts_with("MemTotal") {
+            total_mem = line
+                .replace("MemTotal:", "")
+                .replace("kB", "")
+                .trim()
+                .parse()
+                .unwrap();
+        } else if line.starts_with("MemAvailable") {
+            available_mem = line
+                .replace("MemAvailable:", "")
+                .replace("kB", "")
+                .trim()
+                .parse()
+                .unwrap();
+        }
+    }
+    let mut used_mem: f32 = total_mem - available_mem;
+    let mut mem_unit: String = "KB".to_string();
+    if used_mem > 512000.0 {
+        used_mem /= 1048576.0;
+        total_mem /= 1048576.0;
+        mem_unit = "GB".to_string();
+    } else if used_mem > 1024.0 {
+        used_mem /= 1024.0;
+        total_mem /= 1024.0;
+        mem_unit = "MB".to_string();
+    }
+    let cpu: String = fs::read_to_string("/proc/cpuinfo").expect("Found file!");
+    let mut cpu_model: String = "Unknown".to_string();
+    for line in cpu.lines() {
+        if line.starts_with("model name") {
+            cpu_model = line
+                .replace("model name", "")
+                .replace(':', "")
+                .trim()
+                .to_string();
+        }
+    }
+    let shell: String = if let Ok(stat) = fs::read_to_string("/proc/self/stat") {
+        let values: Vec<&str> = stat.split_whitespace().collect();
+        if values.len() > 3 {
+            fs::read_to_string(format!("/proc/{}/comm", values[3]))
+                .expect("Found file!")
+                .replace("\n", "")
+        } else {
+            "Unknown".to_string()
+        }
+    } else {
+        "Unknown".to_string()
+    };
+    let packages: u32 = count_packages();
+    let gpu: String = get_gpu();
 
     print!("\n{}@{}\n", username.green(), hostname.green());
     for _ in 0..length {
@@ -48,20 +104,72 @@ fn main() {
     println!("\n{}: {}", "OS".green(), os.green());
     println!("{}: {}", "Kernel".green(), kernel.green());
     println!(
-        "{}: {} {} {}",
+        "{}: {}{}{}",
         "Uptime".green(),
         u_hours.green(),
         u_minutes.green(),
         u_seconds.green()
     );
+    println!(
+        "{}: {} {} / {} {} ({}%)",
+        "Memory".green(),
+        ((used_mem * 100.0).round() / 100.0).green(),
+        mem_unit,
+        ((total_mem * 100.0).round() / 100.0).green(),
+        mem_unit,
+        ((used_mem / total_mem * 1000.0).round() / 10.0).green()
+    );
+    println!("{}: {}", "CPU".green(), cpu_model.green());
+    println!("{}: {}", "GPU".green(), gpu.green());
+    println!("{}: {}", "Shell".green(), shell.green());
+    println!("{}: {} (apt)", "Packages".green(), packages.green());
+    println!("");
 }
 
 fn calc_time(uptime: &mut f32, multiplier: f32, name: &str) -> String {
     if *uptime >= multiplier {
         let time = (*uptime / multiplier).floor();
         *uptime -= multiplier * time;
-        format!("{} {}{}", time, name, if time == 1.0 { "" } else { "s" })
+        format!("{} {}{} ", time, name, if time == 1.0 { "" } else { "s" })
     } else {
         "".to_string()
+    }
+}
+
+fn count_packages() -> u32 {
+    if let Ok(packages) = Command::new("dpkg-query")
+        .args(["-f", "'${binary:Package}\n'", "-W"])
+        .output()
+    {
+        if packages.status.success() {
+            return String::from_utf8_lossy(&packages.stdout).lines().count() as u32 - 1;
+        } else {
+            0
+        }
+    } else {
+        0
+    }
+}
+
+fn get_gpu() -> String {
+    if let Ok(lspci) = Command::new("lspci").output() {
+        if lspci.status.success() {
+            let specs = String::from_utf8_lossy(&lspci.stdout);
+            let mut result: String = "".to_string();
+            for line in specs.lines() {
+                if line.to_lowercase().contains("vga") || line.to_lowercase().contains("3d") {
+                    if let Some(text) = line.find("controller:") {
+                        result = line[text + 11..].trim().to_string()
+                    } else {
+                        result = line.to_string();
+                    }
+                }
+            }
+            result
+        } else {
+            "Unknown".to_string()
+        }
+    } else {
+        "Unknown".to_string()
     }
 }
